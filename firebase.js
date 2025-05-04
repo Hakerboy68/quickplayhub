834e6fecf7b3796c6267e9051abe7472c582dd50
Hackerboy68 <163637208+Hakerboy68@users.noreply.github.com>
2025-05-04 14:17:14 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,11 +1,13 @@
 // TODO: Add your Firebase config here
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyAv3MYHvWqTFXW65ydkn_29MeVy7bHPrhU",
+  authDomain: "pokemonrpg2-49db1.firebaseapp.com",
+  databaseURL: "https://pokemonrpg2-49db1-default-rtdb.firebaseio.com",
+  projectId: "pokemonrpg2-49db1",
+  storageBucket: "pokemonrpg2-49db1.firebasestorage.app",
+  messagingSenderId: "571501991226",
+  appId: "1:571501991226:web:b22243bebe6e532bc12c03",
+  measurementId: "G-VB97LQB3MC"
 };
 firebase.initializeApp(firebaseConfig);
